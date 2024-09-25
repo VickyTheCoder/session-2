3b94ae86e7e1167ed69b5183c29fbd7ed2845b91
Vikram.Dhanapalan <vikram.the.coder@gmail.com>
2024-09-25 21:39:01 +0530

    Homepage set for the site

--- JobHunt/settings.py
+++ JobHunt/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'JobHunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- JobHunt/urls.py
+++ JobHunt/urls.py
@@ -15,5 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from ui import views as ui_views
 
-urlpatterns = []
+urlpatterns = [
+    path('', ui_views.homepage),
+]
